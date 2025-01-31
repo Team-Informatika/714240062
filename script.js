47f91c633c67a621a2fc5f9193130f539e5b0b65
MuhammadMallq <Kamalputra1177@gmail.com>
2025-01-31 17:47:51 +0700

    cihuy

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function showMessage() {
-    alert("Terima kasih telah menghubungi Maliq!");
+import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js";
+import { renderHTML } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
+
+renderHTML ("root", "home.html");
+
+function renderDatadariJson() {
+
+    getJSON ("https://t.if.co.id/json/Malik.json", "aja", "enak", responseFunction);
+
 }
